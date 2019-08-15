0e36b8e64e0b2db168fbe71dc8bff053dcb639f2
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-08-15 22:54:12 +0800

    finish matchGenerator

--- matchGene.py
+++ matchGene.py
@@ -1,11 +1,23 @@
 import json
+import time
 
-matches = []
+matches = {}
 
 compLevel = input('Comp level: ')
 event_key = input('Event key: ')
+startFrom = int(input('Match start from: '))
+matchCount = int(input('Total match count: '))
 
-while True:
+for num in range(matchCount):
+    match_number = num+startFrom
+    key = '{}_{}{}'.format(event_key, compLevel, match_number)
+    match = {
+        'time': int(time.time()),
+        'comp_level': compLevel,
+        'event_key': event_key,
+        'key': key,
+        'match_number': match_number
+    }
     alliances = {
         'red': {
             'team_keys': []
@@ -23,4 +35,11 @@ while True:
         team = input('Blue {}: '.format(i+1))
         alliances['blue']['team_keys'].append('frc{}'.format(team))
 
-    print(alliances)
+    match['alliances'] = alliances
+
+    print(match)
+
+    matches[key] = match
+
+with open('generated_matches.json', 'w+') as FILE:
+    json.dump(matches, FILE)
