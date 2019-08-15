c1ab5dea36703cb3682a0a6b7f0306cf09879590
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-08-15 21:14:56 +0800

    0815

--- matchGene.py
+++ matchGene.py
@@ -1,2 +1,26 @@
 import json
 
+matches = []
+
+compLevel = input('Comp level: ')
+event_key = input('Event key: ')
+
+while True:
+    alliances = {
+        'red': {
+            'team_keys': []
+        },
+        'blue': {
+            'team_keys': []
+        }
+    }
+
+    for i in range(3):
+        team = input('Red {}: '.format(i+1))
+        alliances['red']['team_keys'].append('frc{}'.format(team))
+
+    for i in range(3):
+        team = input('Blue {}: '.format(i+1))
+        alliances['blue']['team_keys'].append('frc{}'.format(team))
+
+    print(alliances)
